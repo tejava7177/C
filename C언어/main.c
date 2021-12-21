ae6ec4298ed707bb51debdfbd8b6efbf79591f4f
tejava7177 <juheun9912@gmail.com>
2021-12-22 00:38:19 +0900

    Initial Commit

---
+++ C언어/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C언어
+//
+//  Created by 심주흔 on 2021/12/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
