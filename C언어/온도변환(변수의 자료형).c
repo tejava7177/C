d474025bba8f05e9da894d0817203b6b5c133d98
심주흔 <88835746+tejava7177@users.noreply.github.com>
2021-12-22 01:48:41 +0900

    Add files via upload

---
+++ C언어/온도변환(변수의 자료형).c
@@ -0,0 +1,22 @@
+//
+//  온도변환(변수의 자료형).c
+//  C언어
+//
+//  Created by 심주흔 on 2021/12/22.
+//  쉽게 풀어쓴 C언어 EXPRESS 3장 4번(page.124)
+
+#include <stdio.h>
+
+int main(void){
+    
+    //화씨 온도 값을 받을 변수를 실수형으로 생성 및 0으로 초기화
+    double temperature = 0;
+    printf("Write Fahrenheit temperature : ");
+    scanf("%lf" , &temperature);                //사용자로부터 화씨온도를 받음.
+    
+    temperature = (5.0 / 9.0)*(temperature - 32.0);             //연산과정
+    printf("Temperature in Celsius is %lf\n", temperature);     //섭씨온도 결과값 출력
+    
+   
+    return 0;
+}
