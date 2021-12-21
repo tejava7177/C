a8083679e80662244a54218c9d4afe0a1ae0a89d
심주흔 <88835746+tejava7177@users.noreply.github.com>
2021-12-22 01:53:05 +0900

    Add files via upload

---
+++ C언어/다항식계산.c
@@ -0,0 +1,19 @@
+//
+//  다항식계산.c
+//  C언어
+//
+//  Created by 심주흔 on 2021/12/22.
+//  쉽게 풀어쓴 C언어 EXPRESS 3장 5번(page.125)
+
+#include <stdio.h>
+
+int main(void){
+    
+    int num = 0;
+    printf("Type Num :");
+    scanf("%d" , &num);
+    
+    printf("Result = %d\n" , 3*num*num+7*num+11);
+    
+    return 0;
+}
