92566f3cff1b2fc8a73671500c94902bd9dcb26c
심주흔 <88835746+tejava7177@users.noreply.github.com>
2021-12-24 16:07:49 +0900

    Add files via upload

---
+++ C언어/소수점,지수점표기방법.c
@@ -0,0 +1,19 @@
+//
+//  소수점,지수점표기방법.c
+//  C언어
+//
+//  Created by 심주흔 on 2021/12/22.
+//  쉽게 풀어쓴 C언어 EXPRESS 4장 1번(page.168)
+
+#include <stdio.h>
+
+int main(void){
+    double num = 0;
+    printf("Type num : ");
+    scanf("%lf" , &num);
+    
+    printf("실수 형식으로는 : %lf\n" , num);
+    printf("지수 형식으로는 : %e\n" , num);
+    
+    return 0;
+}
